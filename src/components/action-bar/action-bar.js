de34e4b757ee659ff4dddd30954e4d9f35938079
Davy CHHOUK <chhouk.davy@gmail.com>
2018-11-11 18:06:24 +0700

    Add Icons

---
+++ src/components/action-bar/action-bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const ActionBar = props => {
+  return (
+    <div className="d-flex flex-row mt-3">
+      <div>Action</div>
+    </div>
+  );
+};
+
+export default ActionBar;
--- src/components/post/post.js
+++ src/components/post/post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardText, CardBody, CardLink, CardImg } from 'reactstrap';
 import PostHeader from '../header/post-header';
+import ActionBar from '../action-bar/action-bar';
 
 const Post = props => {
   return (
@@ -14,6 +15,7 @@ const Post = props => {
             src="https://raw.githubusercontent.com/davychhouk/let-redux/master/src/assets/images/luka.png"
             alt="Card image cap"
           />
+          <ActionBar />
           <CardText>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
